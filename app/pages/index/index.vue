55fda28928fba458e762dc528c227b5212f2b52c
yourwold <yourwold@gmail.com>
2025-11-05 23:55:59 +0800

    init

--- app/pages/index/index.vue
+++ app/pages/index/index.vue
@@ -11,36 +11,31 @@ import HomeSwiper from "@/components/Home/swiper/Swiper.vue";
 const route = useRoute();
 const { find, findOne } = useStrapi<Course>();
 
-const { data: homeSwiperData, pending, refresh, error } = await useAsyncData(
-    "homes-swiper",
-    () => find<Restaurant>("homes", "swiper", {
+const { data: homeData, pending, refresh, error } = await useAsyncData("home", () => find<Restaurant>("home", {
+  // populate: ["image"],
+}));
+console.log("homeInfoData的数据是:", homeData.value)
 
-    })
-);
-
-console.log("homeSwiperData的数据是:", homeSwiperData.value)
 
 
 const testData = [
   {
-    images: [
-      {
-        url: "https://www.shuzhitumei.com/_nuxt/swiper1.BoAFuRId.jpg"
-      }
-    ],
+    images: {
+      url: "https://www.shuzhitumei.com/_nuxt/swiper1.BoAFuRId.jpg"
+    },
     title: "途美商旅",
-    desc: ["数智化全场景企业差旅及费控服务平台"],
+    desc: "数智化全场景企业差旅及费控服务平台",
     showBtn: true,
-    tips: [
-      "为100+同业伙伴提供差旅管控模块和出行资源开发对接服务，超2000家各行业头部和高成长型客户差旅及费控服务经验，致力于为客户提供一站式差旅服务，助力企业打造合规节省、规避价格黑箱、智能高效的差旅出行平台。",
-    ],
+    tips: "为100+同业伙伴提供差旅管控模块和出行资源开发对接服务，超2000家各行业头部和高成长型客户差旅及费控服务经验，致力于为客户提供一站式差旅服务，助力企业打造合规节省、规避价格黑箱、智能高效的差旅出行平台。",
   },
 ]
 </script>
 
 <template>
-  homeSwiperData: {{ JSON.stringify(homeSwiperData, 2, null) }}
-  <div></div>
+
+  <div>
+    homeData: {{ homeData }}
+  </div>
 
 
   <HomeSwiper
